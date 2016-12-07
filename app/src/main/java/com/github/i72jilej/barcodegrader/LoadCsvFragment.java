9060f776a054bb8e04e60b412d674c4d6713c17c
Julio Domingo Jiménez Ledesma <i72jilej@uco.es>
2016-12-07 11:55:09 +0100

    Initial commit

---
+++ app/src/main/java/com/github/i72jilej/barcodegrader/LoadCsvFragment.java
@@ -0,0 +1,249 @@
+package com.github.i72jilej.barcodegrader;
+
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.opencsv.CSVReader;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import static android.app.Activity.RESULT_OK;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link LoadCsvFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link LoadCsvFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LoadCsvFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    // Codes for onActivityResult
+    private static final int PICKFILE_RESULT_CODE = 1;
+
+    //Widgets
+    private TextView info_filename;
+    private String info_filename_text = null;
+
+    private Uri inputUri = null;
+    Context applicationContext = null;
+    private ArrayList<String[]> csvArray = new ArrayList<String[]>();
+
+    public LoadCsvFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment LoadCsvFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static LoadCsvFragment newInstance(String param1, String param2) {
+        LoadCsvFragment fragment = new LoadCsvFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+        setRetainInstance(true);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        //http://stackoverflow.com/questions/12026442/setretaininstance-not-retaining-the-instance
+
+        // Inflate the layout for this fragment
+        final View view = inflater.inflate(R.layout.fragment_load_csv, container, false);
+
+        //Loading info labels
+        info_filename = (TextView) view.findViewById(R.id.info_filename);
+
+        //Checking if it's a reload
+        if (info_filename_text != null){
+            info_filename.setText(info_filename_text);
+        }
+
+        //Assigning onClick for loadCsv button
+        Button button_loadCsv = (Button) view.findViewById(R.id.button_loadCsv);
+        button_loadCsv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadCsv(view);
+            }
+        });
+
+        //Assign onClick for showCsv button
+        Button button_showCsv = (Button) view.findViewById(R.id.button_showCsv);
+        button_showCsv.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                System.out.println("OTRO");
+                System.out.println(info_filename_text.toString());
+            }
+        });
+
+        applicationContext = getActivity().getApplicationContext();
+
+
+
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+    //On Click para el botón de cargar csv
+    public void loadCsv(View v) {
+        // Note (eliminar cuando la leais): Esto daba problemas porque
+        // Use ACTION_GET_CONTENT if you want your app to simply read/import data. With this approach, the app imports a _*_copy of the data_*_, such as an image file.
+        // Use ACTION_OPEN_DOCUMENT if you want your app to have long term, persistent access to documents owned by a document provider. An example would be a photo-editing app that lets users edit images stored in a document provider.
+        // puede que si se tiene cuidado al leer de cierta forma funcione bien pues lo que hacemos
+        // es leerlo y olvidarnos pero no se si eso interfiere con los paths o algo.
+        Intent fileintent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        fileintent.addCategory(Intent.CATEGORY_OPENABLE);
+        fileintent.setType("*/*"); //Error con text/plain en Android 6 y con Android 5 tb intermitente?
+
+        try {
+            startActivityForResult(fileintent, PICKFILE_RESULT_CODE);
+        } catch (ActivityNotFoundException e) {
+            e.printStackTrace();
+            //Log.e("tag", "No activity can handle picking a file. Showing alternatives.");
+        }
+
+    }
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode){
+            case PICKFILE_RESULT_CODE:  //After choosing a file in the file explorer
+                System.out.println("EXPLORER CLOSED");
+
+                CSVReader csvFile = null;
+                InputStream inputStream = null;
+                BufferedReader reader = null;
+
+                if(resultCode == RESULT_OK){ //If a file has been chosen
+                    System.out.println("FILE CHOSEN");
+
+                    if(data != null){
+                        inputUri = data.getData();
+
+                        try{
+                            inputStream = applicationContext.getContentResolver().openInputStream(inputUri);
+                            reader = new BufferedReader(new InputStreamReader(inputStream));
+                            csvFile = new CSVReader(reader);
+
+
+                            info_filename_text = inputUri.getPath().toString();
+                            info_filename.setText(info_filename_text);
+
+                            csvArray.clear();
+                            String[] nextLine;
+
+                            try{
+                                while ((nextLine = csvFile.readNext()) != null) {
+                                    // nextLine[] is an array of values from the line
+                                    csvArray.add(nextLine);
+                                    //System.out.println(csvArray.get(0)[0]); // Para mostrar una cadena en concreto: (0) para la linea, [0] para el elemento
+                                }
+                            }catch(IOException e){
+
+                            }
+                        }catch (FileNotFoundException e){
+
+                        }
+                    }
+
+                }
+                else{
+                    System.out.println("NO FILE CHOSEN");
+                }
+                break;
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/github/i72jilej/barcodegrader/MainActivity.java
@@ -0,0 +1,243 @@
+package com.github.i72jilej.barcodegrader;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.net.Uri;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity
+    implements  LoadCsvFragment.OnFragmentInteractionListener,
+                GradeFragment.OnFragmentInteractionListener,
+                CreateCodesFragment.OnFragmentInteractionListener,
+                SettingsFragment.OnFragmentInteractionListener{
+    private DrawerLayout mDrawer;
+    private Toolbar toolbar;
+    private NavigationView nvDrawer;
+
+    LoadCsvFragment loadCsvFragment = null;;
+    FragmentManager fragmentManager = getSupportFragmentManager();
+
+    // Make sure to be using android.support.v7.app.ActionBarDrawerToggle version.
+    // The android.support.v4.app.ActionBarDrawerToggle has been deprecated.
+    private ActionBarDrawerToggle drawerToggle;
+
+    @Override
+    public void onFragmentInteraction(Uri uri){
+        //you can leave it empty
+    }
+
+    public String label_filename_text = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /**
+        // Set a Toolbar to replace the ActionBar.
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Find our drawer view
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+         **/
+
+        // Set a Toolbar to replace the ActionBar.
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Find our drawer view
+        nvDrawer = (NavigationView) findViewById(R.id.nvView);
+        // Setup drawer view
+        setupDrawerContent(nvDrawer);
+
+        // Find our drawer view
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerToggle = setupDrawerToggle();
+
+        // Tie DrawerLayout events to the ActionBarToggle
+        mDrawer.addDrawerListener(drawerToggle);
+
+        // Insert the fragment by replacing any existing fragment
+        Class fragmentClass = LoadCsvFragment.class;
+        //LoadCsvFragment loadCsvFragment = null;;
+        //FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = null;
+
+        if(loadCsvFragment == null)
+            loadCsvFragment = new LoadCsvFragment();
+        fragmentManager.beginTransaction().replace(R.id.flContent, loadCsvFragment).commit();
+
+        /*
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.flContent, fragment).commit();
+        */
+
+        contextOfApplication = getApplicationContext();
+
+
+    }
+
+    private ActionBarDrawerToggle setupDrawerToggle() {
+        // NOTE: Make sure you pass in a valid toolbar reference.  ActionBarDrawToggle() does not require it
+        // and will not render the hamburger icon without it.
+        return new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.drawer_open,  R.string.drawer_close);
+    }
+
+    // `onPostCreate` called when activity start-up is complete after `onStart()`
+    // NOTE 1: Make sure to override the method with only a single `Bundle` argument
+    // Note 2: Make sure you implement the correct `onPostCreate(Bundle savedInstanceState)` method.
+    // There are 2 signatures and only `onPostCreate(Bundle state)` shows the hamburger icon.
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggles
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        /**
+        // The action bar home/up action should open or close the drawer.
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawer.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+         **/
+
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        /**
+        // Create a new fragment and specify the fragment to show based on nav item clicked
+        Fragment fragment = null;
+        Class fragmentClass;
+        switch(menuItem.getItemId()) {
+            case R.id.nav_fragment_loadCsv:
+                fragmentClass = LoadCsvFragment.class;
+                break;
+            case R.id.nav_fragment_grade:
+                fragmentClass = GradeFragment.class;
+                break;
+            case R.id.nav_fragment_createCodes:
+                fragmentClass = CreateCodesFragment.class;
+                break;
+            case R.id.nav_fragment_settings:
+                fragmentClass = SettingsFragment.class;
+                break;
+            default:
+                fragmentClass = LoadCsvFragment.class;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+         */
+
+        Fragment fragment = null;
+        //LoadCsvFragment loadCsvFragment = null;
+        //FragmentManager fragmentManager = getSupportFragmentManager();
+        GradeFragment gradeFragment = null;
+        CreateCodesFragment createCodesFragment = null;
+        SettingsFragment settingsFragment = null;
+        switch(menuItem.getItemId()){
+            case R.id.nav_fragment_loadCsv:
+                if(loadCsvFragment == null)
+                    loadCsvFragment = new LoadCsvFragment();
+
+                fragmentManager.beginTransaction().replace(R.id.flContent, loadCsvFragment).commit();
+                break;
+            case R.id.nav_fragment_grade:
+                if(gradeFragment == null)
+                    gradeFragment = new GradeFragment();
+
+                fragmentManager.beginTransaction().replace(R.id.flContent, gradeFragment).commit();
+                break;
+            case R.id.nav_fragment_createCodes:
+                if(createCodesFragment == null)
+                    createCodesFragment = new CreateCodesFragment();
+
+                fragmentManager.beginTransaction().replace(R.id.flContent, createCodesFragment).commit();
+                break;
+            case R.id.nav_fragment_settings:
+                if(settingsFragment == null)
+                    settingsFragment = new SettingsFragment();
+
+                fragmentManager.beginTransaction().replace(R.id.flContent, settingsFragment).commit();
+                break;
+            default:
+                if(loadCsvFragment == null)
+                    loadCsvFragment = new LoadCsvFragment();
+
+                fragmentManager.beginTransaction().replace(R.id.flContent, loadCsvFragment).commit();
+        }
+
+        // Highlight the selected item has been done by NavigationView
+        menuItem.setChecked(true);
+        // Set action bar title
+        setTitle(menuItem.getTitle());
+        // Close the navigation drawer
+        mDrawer.closeDrawers();
+    }
+
+    public static Context contextOfApplication;
+    public static Context getContextOfApplication()
+    {
+        return contextOfApplication;
+    }
+
+    public void setLabel_filename_text(String value){
+        label_filename_text = value;
+    }
+
+    public String getLabel_filename_text(){
+        return label_filename_text;
+    }
+
+}
